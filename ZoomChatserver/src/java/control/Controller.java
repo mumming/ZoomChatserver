91fa91cacf939d9cf9ef11d3fa0319a52ec3f6cc
Martin Nielsen <martinnielsen92@hotmail.com>
2016-05-13 10:39:32 +0200

    andet commit

---
+++ ZoomChatserver/src/java/control/Controller.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+import java.util.ArrayList;
+/**
+ *
+ * @author Martin
+ */
+public class Controller {
+    public static ArrayList<String> messages;
+    public Controller(){
+        messages = new ArrayList<>();
+    }
+}
